3524910f852c7d1cb09c7ae9d59620340b36e028
LucasCosta96RS <lucascosta96rs@gmail.com>
2023-06-22 21:46:52 -0300

    Input agora aparece o valor monetário na hora de digitar

--- scripts.js
+++ scripts.js
@@ -1,74 +1,63 @@
 const convertButton = document.querySelector(".convert-button")
-
-let currencyInput = document.querySelector(".currency-input")
-
-const currencyInputValue = document.querySelector(".currency-input").value
-    
-
-function convertValues() {
-    
-    const selectedOption = document.querySelector(".select-to").value
-
-    let initialValue = document.querySelector(".initial-value")
-
-    let finalValue = document.querySelector(".final-value")
-
-    let finalCurrency = document.querySelector(".final-currency")
-
-    let finalCurrencyImage = document.querySelector(".final-currency-img")
-    
-    const dolarToday = 5
-
-    const euroToday = 6.2
-
-    const convertedValueDolar = currencyInputValue / dolarToday
-
-    const convertedValueEuro = currencyInputValue / euroToday
-
-    let Real = new Intl.NumberFormat("pt-BR", {style: 'currency',
-    currency: 'BRL',})
-
-    let Dolar = new Intl.NumberFormat("en-US", {style: 'currency',
-    currency: 'USD',})
-
-    let Euro = new Intl.NumberFormat("en-DE", {style: 'currency',
-    currency: 'EUR',})
-
-    if (selectedOption == "Dolar") {
-
-        initialValue.innerHTML = Real.format(currencyInputValue)
-        finalValue.innerHTML = Dolar.format(convertedValueDolar)
-
-    } else if (selectedOption == "Euro") {
-
-        finalCurrencyImage.src = "./assets/Euro.png"
-        finalCurrency.innerHTML = "Euro"
-        initialValue.innerHTML = Real.format(currencyInputValue)
-        finalValue.innerHTML = Euro.format(convertedValueEuro)
-
-    }
-
-    
-}
-
-convertButton.addEventListener("click", convertValues)
+const currencyInput = document.getElementById("currency-input")
 
 function formatarMoeda(valor) {
-    const valorNumerico = parseFloat(valor) / 100;
+    const numeroLimpo = valor.replace(/\D/g, '')
+    const valorNumerico = parseFloat(numeroLimpo) / 100
     const valorFormatado = valorNumerico.toLocaleString('pt-BR', {
       style: 'currency',
       currency: 'BRL',
-    });
-    return valorFormatado;
-  }
+    })
+    return valorFormatado
+}
 
-  currencyInput = document.getElementsByClassName("currency-input");
+currencyInput.addEventListener("input", function (event) {
+    const valorDigitado = event.target.value
+    const valorFormatado = formatarMoeda(valorDigitado)
+    event.target.value = valorFormatado
+})  
 
-  currencyInput.addEventListener('input', function (input) {
-    const valorDigitado = input.target.value;
-    const valorFormatado = formatarMoeda(valorDigitado);
-    input.target.value = valorFormatado;
-  });
+function limparMoeda(valor) {
+    const numeroLimpo = valor.replace(/\D/g, '');
+    return numeroLimpo / 100;
+}
+
+function convertValues() {
+    const selectedOption = document.querySelector(".select-to").value
+    const inputValue = currencyInput.value
+    const valorLimpo = limparMoeda(inputValue)
+
+    let initialValue = document.querySelector(".initial-value")
+    let finalValue = document.querySelector(".final-value")
+    let finalCurrency = document.querySelector(".final-currency")
+    let finalCurrencyImage = document.querySelector(".final-currency-img")
+    
+    const dolarToday = 5
+    const euroToday = 6.2
+
+    const convertedValueDolar = valorLimpo / dolarToday
+    const convertedValueEuro = valorLimpo / euroToday
+
+    let Real = new Intl.NumberFormat("pt-BR", {style: 'currency', currency: 'BRL',})
+    let Dolar = new Intl.NumberFormat("en-US", {style: 'currency', currency: 'USD',})
+    let Euro = new Intl.NumberFormat("en-DE", {style: 'currency', currency: 'EUR',})
+
+    if (selectedOption == "Dolar") {
+        initialValue.innerHTML = Real.format(valorLimpo)
+        finalValue.innerHTML = Dolar.format(convertedValueDolar)
+    } else if (selectedOption == "Euro") {
+        finalCurrencyImage.src = "./assets/Euro.png"
+        finalCurrency.innerHTML = "Euro"
+        initialValue.innerHTML = Real.format(valorLimpo)
+        finalValue.innerHTML = Euro.format(convertedValueEuro)
+    }
+} 
+
+convertButton.addEventListener("click", convertValues)
+
+
+
+
 
 
 
