db423ac54cab42257f8858708cb70c36b648e74d
LucasCosta96RS <lucascosta96rs@gmail.com>
2023-07-10 16:04:29 -0300

    added exchangerate-api

--- scripts.js
+++ scripts.js
@@ -22,26 +22,45 @@ function limparMoeda(valor) {
     return numeroLimpo / 100;
 }
 
-function convertValues() {
+async function obterTaxaDeCambio(moeda) {
+  try {
+    let url;
+    if (moeda == 'Dolar') {
+      url = 'https://api.exchangerate-api.com/v4/latest/USD';
+    } else if (moeda == 'Euro') {
+      url = 'https://api.exchangerate-api.com/v4/latest/EUR';
+    } else {
+      throw new Error('Moeda não suportada');
+    }
+
+    const response = await fetch(url);
+    const data = await response.json();
+    const taxaCambio = data.rates.BRL;
+    return taxaCambio;
+  } catch (error) {
+    console.log('Erro ao obter a taxa de câmbio:', error);
+  }
+}
+
+async function convertValues() {
     const selectedOption = document.querySelector(".select-to").value
     const inputValue = currencyInput.value
     const valorLimpo = limparMoeda(inputValue)
+    const taxaCambio = await obterTaxaDeCambio(selectedOption);
 
     let initialValue = document.querySelector(".initial-value")
     let finalValue = document.querySelector(".final-value")
     let finalCurrency = document.querySelector(".final-currency")
     let finalCurrencyImage = document.querySelector(".final-currency-img")
-    
-    const dolarToday = 5
-    const euroToday = 6.2
 
-    const convertedValueDolar = valorLimpo / dolarToday
-    const convertedValueEuro = valorLimpo / euroToday
+    const convertedValueDolar = valorLimpo / taxaCambio
+    const convertedValueEuro = valorLimpo / taxaCambio
 
     let Real = new Intl.NumberFormat("pt-BR", {style: 'currency', currency: 'BRL',})
     let Dolar = new Intl.NumberFormat("en-US", {style: 'currency', currency: 'USD',})
     let Euro = new Intl.NumberFormat("en-DE", {style: 'currency', currency: 'EUR',})
 
+    
     if (selectedOption == "Dolar") {
         initialValue.innerHTML = Real.format(valorLimpo)
         finalValue.innerHTML = Dolar.format(convertedValueDolar)
